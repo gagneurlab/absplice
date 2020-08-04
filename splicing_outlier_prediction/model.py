09d249c3d0547769b18ac697225c264fb2ba2577
M . Hasan Celik <muhammedhasancelik@gmail.com>
2020-08-04 20:21:09 +0200

    sample options added

--- splicing_outlier_prediction/model.py
+++ splicing_outlier_prediction/model.py
@@ -1,12 +1,24 @@
-from mmsplice.junction_dataloader import JunctionPSI5VCFDataloader, JunctionPSI3VCFDataloader
+from kipoi.data import SampleIterator
+from mmsplice.junction_dataloader import JunctionPSI5VCFDataloader, \
+    JunctionPSI3VCFDataloader
 from mmsplice import MMSplice
 
 
-class SpliceOutlierPrediction:
+class SpliceOutlierDataloader(SampleIterator):
 
-    def __init__(self, ref_table, fasta_file, vcf_file):
-        self.dl5 = JunctionPSI5VCFDataloader()
-        self.dl3 = JunctionPSI3VCFDataloader()
+    def __init__(self, ref_table, fasta_file, vcf_file, **kwargs):
+        self.dl5 = JunctionPSI5VCFDataloader(
+            ref_table, fasta_file, vcf_file, **kwargs)
+        self.dl3 = JunctionPSI3VCFDataloader(
+            ref_table, fasta_file, vcf_file, **kwargs)
+
+    def __next__(self):
+        pass
+
+
+class SpliceOutlier:
+
+    def __init__(self):
         self.mmsplice = MMSplice()
 
     def predict_on_batch(batch):
