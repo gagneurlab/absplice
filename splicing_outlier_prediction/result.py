3b8c45257d4d26828d2c026f03fd8ad1cc21e1b5
Nils Wagner <wagnern@in.tum.de>
2022-01-21 02:42:37 +0100

    infer cat asserts for common junctions

--- splicing_outlier_prediction/result.py
+++ splicing_outlier_prediction/result.py
@@ -191,15 +191,25 @@ class SplicingOutlierResult:
         infer_rows = list()
         for cat in cat_inference:
             assert self.contains_chr == cat.contains_chr
-            rows = self.junction.iterrows()
+            
+            common_junctions = set.union(*cat.common_junctions3)\
+                        .union(set.union(*cat.common_junctions5))
+            df_common_junctions = self.junction[
+                self.junction.index.get_level_values('junction').isin(common_junctions)]
+            assert df_common_junctions.shape[0] > 0
+            rows = df_common_junctions.iterrows()
+            
+            # rows = self.junction.iterrows()
             if progress:
-                rows = tqdm(rows, total=self.junction.shape[0])
+                rows = tqdm(rows, total=df_common_junctions.shape[0])
+                # rows = tqdm(rows, total=self.junction.shape[0])
             for (junction, tissue, sample), row in rows:
                 if cat.contains(junction, tissue, sample, row['event_type']):
                     infer_rows.append(
                         cat.infer(junction, tissue, sample, row['event_type']))
         df = pd.DataFrame(infer_rows)
         assert df.shape[0] > 0
+        assert len(set(df_common_junctions.index.get_level_values('junction')).difference(set(df['junction']))) == 0
         df = df.drop_duplicates().set_index(['junction', 'tissue', 'sample'])
         # self._junction can contain multiple cats (junction, tissue, sample) is not unique index
         self._junction = self.junction.join(df)
